1e567f998f31a42239f282bb6444c944c78a57f4
Josh Broton <joshbroton@Joshs-MacBook-Pro.local>
2013-12-05 16:53:29 -0600

    Start work on header

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,9 +1,7 @@
 <form role="search" method="get" class="search-form form-inline" action="<?php echo home_url('/'); ?>">
-  <div class="input-group">
-    <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'spring'); ?> <?php bloginfo('name'); ?>">
-    <label class="hide"><?php _e('Search for:', 'spring'); ?></label>
-    <span class="input-group-btn">
-      <button type="submit" class="search-submit btn btn-default"><?php _e('Search', 'spring'); ?></button>
-    </span>
-  </div>
+    <div class="input-group">
+        <label for="txtSearch"><?php _e('Search for:', 'spring'); ?></label>
+        <input id="txtSearch" type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'spring'); ?> <?php bloginfo('name'); ?>">
+        <button type="submit" class="search-button"><?php _e('Search', 'spring'); ?></button>
+    </div>
 </form>
