2af3bc2c75706a1c6b791fc6e091aee6ebfc3bd3
Kiko Doran <kiko.doran@gmail.com>
2013-12-05 21:05:54 -0600

    Removed notes left in comments

--- functions.php
+++ functions.php
@@ -3,11 +3,11 @@
  * Spring includes
  */
 require_once locate_template('/lib/utils.php');           // Utility functions
-require_once locate_template('/lib/init.php');            // kdInitial theme setup and constants
+require_once locate_template('/lib/init.php');            // Initial theme setup and constants
 require_once locate_template('/lib/wrapper.php');         // Theme wrapper class
 require_once locate_template('/lib/sidebar.php');         // Sidebar class
 require_once locate_template('/lib/config.php');          // Configuration
-require_once locate_template('/lib/titles.php');          // kdPage titles
+require_once locate_template('/lib/titles.php');          // Page titles
 require_once locate_template('/lib/cleanup.php');         // Cleanup
 require_once locate_template('/lib/comments.php');        // Custom comments modifications
 require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
