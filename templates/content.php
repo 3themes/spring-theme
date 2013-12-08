948a9d6816e438f6e9c4d6d08c9c89354489b2a6
Kiko Doran <kiko.doran@gmail.com>
2013-12-07 20:28:21 -0600

    Switched content to show the_content('Read more...') instead of the_excerpt()

--- templates/content.php
+++ templates/content.php
@@ -4,6 +4,6 @@
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
-    <?php the_excerpt(); ?>
+    <?php the_content('Read more...'); ?>
   </div>
 </article>
