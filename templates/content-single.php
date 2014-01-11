ce5ab490ed35347e2958af485cabcf3e32d54704
Josh Broton <joshbroton@Joshs-MacBook-Pro.local>
2014-01-11 14:34:55 -0600

    Add post metadata to the single page

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,15 +1,21 @@
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'spring'), 'after' => '</p></nav>')); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
-  </article>
+    <article <?php post_class(); ?>>
+        <header >
+            <h1 class="post--title"><?php the_title(); ?></h1>
+            <?php get_template_part('templates/entry-meta'); ?>
+        </header>
+        <div class="post--content">
+            <?php the_content(); ?>
+        </div>
+        <footer class="post--metadata">
+            <section class="post--metadata_categories">
+                <span class="post--metadata--title"><?php _e('Categories: ', 'spring'); ?></span> <?php the_category(', '); ?>
+            </section>
+            <section class="post--metadata_tags">
+                <?php the_tags('<span class="post--metadata--title">Tags:</span> ', ', ', ''); ?>
+            </section>
+        </footer>
+        <?php wp_link_pages(array('before' => '<nav class="nav-pager"><p>' . __('Pages:', 'spring'), 'after' => '</p></nav>')); ?>
+        <?php comments_template('/templates/comments.php'); ?>
+    </article>
 <?php endwhile; ?>
--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,3 +1,3 @@
 <section class="post--byline">
-    <span class="byline author vcard"><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></span> &#8226; <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+    <span class="byline--author author vcard"><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></span> &#8226; <time class="byline--date published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 </section>
