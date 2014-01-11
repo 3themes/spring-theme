b9e183cc7f816b03e567838901c672086d8a0894
Josh Broton <joshbroton@Joshs-MacBook-Pro.local>
2014-01-11 01:13:45 -0600

    Update markup for comments

--- templates/comments.php
+++ templates/comments.php
@@ -4,21 +4,21 @@
   }
 
  if (have_comments()) : ?>
-  <section id="comments">
-    <h3><?php printf(_n('One Response to &ldquo;%2$s&rdquo;', '%1$s Responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'spring'), number_format_i18n(get_comments_number()), get_the_title()); ?></h3>
+  <section class="comments" id="comments">
+    <h3 class="comments--title"><?php printf(_n('One Response to &ldquo;%2$s&rdquo;', '%1$s Responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'spring'), number_format_i18n(get_comments_number()), get_the_title()); ?></h3>
 
-    <ol class="media-list">
+    <ol class="comments--list">
       <?php wp_list_comments(array('walker' => new spring_Walker_Comment)); ?>
     </ol>
 
     <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
     <nav>
-      <ul class="pager">
+      <ul class="comments--pager pager">
         <?php if (get_previous_comments_link()) : ?>
-          <li class="previous"><?php previous_comments_link(__('&larr; Older comments', 'spring')); ?></li>
+          <li class="pager--previous"><?php previous_comments_link(__('&larr; Older comments', 'spring')); ?></li>
         <?php endif; ?>
         <?php if (get_next_comments_link()) : ?>
-          <li class="next"><?php next_comments_link(__('Newer comments &rarr;', 'spring')); ?></li>
+          <li class="pager--next"><?php next_comments_link(__('Newer comments &rarr;', 'spring')); ?></li>
         <?php endif; ?>
       </ul>
     </nav>
@@ -41,9 +41,9 @@
 <?php endif; ?>
 
 <?php if (comments_open()) : ?>
-  <section id="respond">
+  <section class="comments--respond" id="respond">
     <h3><?php comment_form_title(__('Leave a Reply', 'spring'), __('Leave a Reply to %s', 'spring')); ?></h3>
-    <p class="cancel-comment-reply"><?php cancel_comment_reply_link(); ?></p>
+    <p class="comments--cancel-reply"><?php cancel_comment_reply_link(); ?></p>
     <?php if (get_option('comment_registration') && !is_user_logged_in()) : ?>
       <p><?php printf(__('You must be <a href="%s">logged in</a> to post a comment.', 'spring'), wp_login_url(get_permalink())); ?></p>
     <?php else : ?>
@@ -54,24 +54,26 @@
             <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php __('Log out of this account', 'spring'); ?>"><?php _e('Log out &raquo;', 'spring'); ?></a>
           </p>
         <?php else : ?>
-          <div class="form-group">
-            <label for="author"><?php _e('Name', 'spring'); if ($req) _e(' (required)', 'spring'); ?></label>
-            <input type="text" class="form-control" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" <?php if ($req) echo 'aria-required="true"'; ?>>
+          <div class="comments--form-row form-row">
+            <label for="commentsAuthor"><?php _e('Name', 'spring'); if ($req) _e(' (required)', 'spring'); ?></label>
+            <input type="text" placeholder="Your name..." class="textbox" name="author" id="commentsAuthor" value="<?php echo esc_attr($comment_author); ?>" size="22" <?php if ($req) echo 'aria-required="true"'; ?>>
           </div>
-          <div class="form-group">
-            <label for="email"><?php _e('Email (will not be published)', 'spring'); if ($req) _e(' (required)', 'spring'); ?></label>
-            <input type="email" class="form-control" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" <?php if ($req) echo 'aria-required="true"'; ?>>
+          <div class="comments--form-row form-row">
+            <label for="commentsEmail"><?php _e('Email (will not be published)', 'spring'); if ($req) _e(' (required)', 'spring'); ?></label>
+            <input type="email" placeholder="Your email address..." class="textbox" name="email" id="commentsEmail" value="<?php echo esc_attr($comment_author_email); ?>" size="22" <?php if ($req) echo 'aria-required="true"'; ?>>
           </div>
-          <div class="form-group">
-            <label for="url"><?php _e('Website', 'spring'); ?></label>
-            <input type="url" class="form-control" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22">
+          <div class="comments--form-row form-row">
+            <label for="commentsUrl"><?php _e('Website', 'spring'); ?></label>
+            <input type="url" placeholder="Your homepage..." class="textbox" name="url" id="commentsUrl" value="<?php echo esc_attr($comment_author_url); ?>" size="22">
           </div>
         <?php endif; ?>
-        <div class="form-group">
-          <label for="comment"><?php _e('Comment', 'spring'); ?></label>
-          <textarea name="comment" id="comment" class="form-control" rows="5" aria-required="true"></textarea>
+        <div class="comments--form-row form-row>
+          <label for="commentsComment"><?php _e('Comment', 'spring'); ?></label>
+          <textarea name="comment" placeholder="The text of your comment..." id="commentsComment" class="form-control" rows="5" aria-required="true"></textarea>
         </div>
-        <p><input name="submit" class="btn btn-primary" type="submit" id="submit" value="<?php _e('Submit Comment', 'spring'); ?>"></p>
+        <div class="button-wrapper">
+            <input name="submit" class="button" type="submit" id="submit" value="<?php _e('Submit Comment', 'spring'); ?>">
+        </div>
         <?php comment_id_fields(); ?>
         <?php do_action('comment_form', $post->ID); ?>
       </form>
