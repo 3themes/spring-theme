d7988dbd5d32113199c64217ca12ef41e1113b30
Josh Broton <joshbroton@Joshs-MacBook-Pro.local>
2014-01-11 01:56:57 -0600

    change class of menu to main-nav--menu

--- templates/header.php
+++ templates/header.php
@@ -78,7 +78,7 @@
         <nav class="nav-main" role="navigation">
             <?php
             if (has_nav_menu('primary_navigation')) :
-                wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'main-menu'));
+                wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav-main--menu'));
             endif;
             ?>
         </nav>
