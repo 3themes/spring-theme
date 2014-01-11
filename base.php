f7018d1b9146da154639f98a6196f55c2489921c
Josh Broton <joshbroton@Joshs-MacBook-Pro.local>
2014-01-11 14:12:11 -0600

    update classes and styles of sidebar

--- base.php
+++ base.php
@@ -1,14 +1,15 @@
 <?php get_template_part('templates/header'); ?>
-<div class="content row">
-<main class="main <?php echo spring_main_class(); ?>" role="main">
-  <?php include spring_template_path(); ?>
-</main><!-- /.main -->
-<?php if (spring_display_sidebar()) : ?>
-  <aside class="sidebar <?php echo spring_sidebar_class(); ?>" role="complementary">
-    <?php include spring_sidebar_path(); ?>
-  </aside><!-- /.sidebar -->
-<?php endif; ?>
-</div><!-- /.content -->
+<section class="content row">
+    <main class="content--body <?php echo spring_main_class(); ?>" role="main">
+        <?php include spring_template_path(); ?>
+    </main>
+    <!-- /.main -->
+    <?php if (spring_display_sidebar()) : ?>
+        <aside class="sidebar content--sidebar <?php echo spring_sidebar_class(); ?>" role="complementary">
+            <?php include spring_sidebar_path(); ?>
+        </aside><!-- /.sidebar -->
+    <?php endif; ?>
+</section><!-- /.content -->
 
 
 <?php get_template_part('templates/footer'); ?>
