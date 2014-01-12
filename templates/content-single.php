5c2436b872fdc5fcd344ab7bb9899e7d6661cc77
Kiko Doran <kiko.doran@gmail.com>
2014-01-11 16:31:19 -0800

    Turned h1 for title back on

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,7 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
     <article <?php post_class(); ?>>
         <header >
-            <!-- <h1 class="post--title"><?php the_title(); ?></h1> -->
+            <h1 class="post--title"><?php the_title(); ?></h1>
             <?php get_template_part('templates/entry-meta'); ?>
         </header>
         <div class="post--content">
