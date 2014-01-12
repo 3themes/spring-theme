108bcbf58dadfede696ac6fec636d755b8b9f577
Kiko Doran <kiko.doran@gmail.com>
2014-01-11 17:23:06 -0800

    Set feature image size to 500x370

--- lib/init.php
+++ lib/init.php
@@ -15,7 +15,7 @@ function spring_setup() {
 
   // Add post thumbnails (http://codex.wordpress.org/Post_Thumbnails)
   add_theme_support('post-thumbnails');
-  // set_post_thumbnail_size(150, 150, false);
+  set_post_thumbnail_size(500, 370, false);
   // add_image_size('category-thumb', 300, 9999); // 300px wide (and unlimited height)
 
   // Add post formats (http://codex.wordpress.org/Post_Formats)
