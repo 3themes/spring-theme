083156011662c69e27cf3c45c224d34b45641342
Kiko Doran <kiko.doran@gmail.com>
2014-01-13 16:23:54 -0800

    moved custom code out of functions.php

--- functions.php
+++ functions.php
@@ -15,10 +15,3 @@ require_once locate_template('/lib/widgets.php');         // Sidebars and widget
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/custom.php');          // Custom functions
 
-add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
-add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
-
-function remove_width_attribute( $html ) {
-    $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
-    return $html;
-}
--- lib/custom.php
+++ lib/custom.php
@@ -6,3 +6,10 @@
 //Comment out the following line to enable the admin bar
 // add_filter('show_admin_bar', '__return_false');
 
+add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+
+function remove_width_attribute( $html ) {
+    $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+    return $html;
+}
