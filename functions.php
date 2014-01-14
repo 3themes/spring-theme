548c1e8cdbabd9b34bdb4487a8300183701deb8d
Kiko Doran <kiko.doran@gmail.com>
2014-01-13 16:25:35 -0800

    removed blank space

--- functions.php
+++ functions.php
@@ -14,4 +14,3 @@ require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/custom.php');          // Custom functions
-
