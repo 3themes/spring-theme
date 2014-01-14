111e36278963b897d7bec2b5328573fd2ebf6ceb
Josh Broton <joshbroton@Joshs-MacBook-Pro.local>
2014-01-13 22:40:59 -0600

    This is Spring Theme statement

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer-main" role="contentinfo">
 	<aside class="footer--copyright">
-        &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
+        &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> &#8226; This is <a href="https://github.com/3themes/spring-theme" target="_blank">Spring Theme</a>, and it's under development by <a href="http://3them.es" target="_blank">3themes</a>. Join the party on <a href="https://github.com/3themes/spring-theme" target="_blank">Github</a>.
 	</aside>
     <nav class="nav-footer" role="navigation">
         <?php
