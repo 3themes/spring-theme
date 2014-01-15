13856a04cd5c6bd34ab0c205dac82be24e59121b
Josh Broton <joshbroton@Joshs-MacBook-Pro.local>
2014-01-15 11:53:56 -0600

    update search markup

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,6 +1,6 @@
 <section class="search" role="search">
     <form role="search" method="get" class="search--form" action="<?php echo home_url('/'); ?>">
-        <div class="input-group">
+        <div class="form--group">
             <label for="txtSearch"><?php _e('Search for:', 'spring'); ?></label>
             <input id="txtSearch" type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="textbox search--textbox" placeholder="<?php _e('Search', 'spring'); ?> <?php bloginfo('name'); ?>">
             <button type="submit" class="button search--button"><?php _e('Search', 'spring'); ?></button>
