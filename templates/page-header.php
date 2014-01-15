d8c2d2b034c4fd3b004b717ca84cb39da286f51f
Josh Broton <joshbroton@Joshs-MacBook-Pro.local>
2014-01-15 11:52:38 -0600

    update markup

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,5 @@
-<div class="page-header">
-  <h1>
-    <?php echo spring_title(); ?>
-  </h1>
-</div>
+<header class="page--header">
+    <h1 class="page--title">
+        <?php echo spring_title(); ?>
+    </h1>
+</header>
